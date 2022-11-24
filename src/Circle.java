a854e1dddae44ee6341127104905ad765a4f877a
escuela@davinci.edu.ar <ignacio.armendariz@davinci.edu.ar>
2022-11-24 09:58:47 -0300

    feat: defined shape

--- src/Circle.java
+++ src/Circle.java
@@ -1,5 +1,16 @@
 public class Circle {
     private double radius;
+    Circle(){
+
+    }
+    Circle(double radius){
+        this.radius(1.0);
+    }
+    Circle(double radius,String color ){
+
+    }
+    private void radius(double v) {
+    }
 
 
     public double getRadius() {
--- src/Shape.java
+++ src/Shape.java
@@ -1,6 +1,34 @@
 abstract class Shape {
     public String color;
     public boolean isFilled;
+    public double area;
+    public double perimeter;
+    public String text;
+
+
+
+    Shape (){
+
+    }
+    Shape(String color , boolean isFilled){
+
+    }
+    public String getColor(){
+      return color;
+}
+
+    public void setColor(String color){
+        this.color="red";
+}
+
+    public boolean isFilled(){
+        return isFilled;
+}
+    public void setisFilled(boolean isFilled){
+        this.isFilled=true;
+}
+    abstract double getArea();
+    abstract double getPerimeter();
 
 
 
