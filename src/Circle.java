e1d94d79ec8f0efbe8ea85454a6375906833c249
escuela@davinci.edu.ar <ignacio.armendariz@davinci.edu.ar>
2022-11-24 10:15:52 -0300

    feat: defined Circle class

--- src/Circle.java
+++ src/Circle.java
@@ -1,35 +1,38 @@
 public class Circle extends Shape {
-
     private double radius;
-    Circle(){
-
-    }
 
     @Override
     double getArea() {
+
         return 0;
     }
 
     @Override
     double getPerimeter() {
+
         return 0;
     }
 
+    Circle(){
+       this.radius=1.0;
+    }
     Circle(double radius){
-        this.radius(1.0);
-    }
-    Circle(double radius,String color ){
 
+        this.radius=radius;
     }
-    private void radius(double v) {
+    Circle(double radius,String color, boolean isFilled){
+        this.radius=radius;
+        this.color=color;
+        this.isFilled=isFilled;
     }
-
 
     public double getRadius() {
+
         return radius;
     }
 
     public void setRadius(double radius) {
+
         this.radius = radius;
     }
 }
