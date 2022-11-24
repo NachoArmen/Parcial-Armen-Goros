359fda2fa235adaab932c0c85b4000651d264599
escuela@davinci.edu.ar <ignacio.armendariz@davinci.edu.ar>
2022-11-24 10:08:29 -0300

    feat: defined rectangle class

--- src/Circle.java
+++ src/Circle.java
@@ -1,8 +1,20 @@
-public class Circle {
+public class Circle extends Shape {
+
     private double radius;
     Circle(){
 
     }
+
+    @Override
+    double getArea() {
+        return 0;
+    }
+
+    @Override
+    double getPerimeter() {
+        return 0;
+    }
+
     Circle(double radius){
         this.radius(1.0);
     }
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,3 +1,34 @@
-public class Rectangle {
+public class Rectangle extends Shape {
 
+   public double width;
+   public double length;
+
+
+   Rectangle(){
+    this.width=1.0;
+    this.length=1.0;
+
+   }
+
+   Rectangle(double width, double length){
+       this.width=width;
+       this.length=length;
+   }
+
+   Rectangle(double width, double length, String color, boolean isFilled){
+       this.width=width;
+       this.length=length;
+       this.color=color;
+       this.isFilled=isFilled;
+   }
+
+    @Override
+    double getArea() {
+        return 0;
+    }
+
+    @Override
+    double getPerimeter() {
+        return 0;
+    }
 }
